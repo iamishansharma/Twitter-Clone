08f17465233428ed6a6c4814a3d315f2a67bc1f3
Ishan Sharma <sharmaishan747@gmail.com>
2021-11-07 19:43:49 +0530

    java formatting

--- backend/Tweet.java
+++ backend/Tweet.java
@@ -1,7 +1,5 @@
-public class Tweet
-{
-    public static void main(String[] args)
-    {
+public class Tweet {
+    public static void main(String[] args) {
         System.out.println("this will be backend");
     }
 }
