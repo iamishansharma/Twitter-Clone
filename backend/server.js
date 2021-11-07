7242267034de7f2b6ceb3d5da001ff01b2da690f
Ishan Sharma <sharmaishan747@gmail.com>
2021-11-07 19:39:37 +0530

    removing backend folder

--- backend/server.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const app = express();
-const mongoose = require("./Database");
-
-var listener = app.listen(4000, () => {
-  console.log(
-    `Twitter Clone Backend Running on Port: ${listener.address().port}`
-  );
-});
